0a51a935a99ed326c26e4a7193b30b4074ad31f0
KidCoderT <Tejas75O25@gmail.com>
2020-11-02 16:25:49 +0530

    Ex 1.7: unicafe (Step 2) (Part D)

--- part1/src/index.js
+++ part1/src/index.js
@@ -5,23 +5,46 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const IncrementGood = () => () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+
+  const IncrementNeutral = () => () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const IncrementBad = () => () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
+
+  const GetAverage = () => (good + 0 + bad * -1)/all
+
+  const GetPositivePercent = () => good/all * 100
 
   return (
     <div>
       <h1>give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={IncrementGood()}>good</button>
+      <button onClick={IncrementNeutral()}>neutral</button>
+      <button onClick={IncrementBad()}>bad</button>
 
       <h1>statistics</h1>
 
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {GetAverage()}</div>
+      <div>positive {GetPositivePercent()} %</div>
     </div>
   )
-}
+ }
 
 ReactDOM.render(
   <App />,
