4293251e9dd2ba054d14d4fc4f5de080863ffda6
KidCoderT <Tejas75O25@gmail.com>
2020-11-02 13:09:02 +0530

    Learning A more complex state, debugging React apps (D)

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 // This is the Ex Code and I will uncomment them once I start doing the Ex files again
 // const Header = (props) => {
 //   return (
-//     <h1>{props.course.name}</h1>
+//     <h1>{props.course.name}</h1> 
 //   )
 // }
 
@@ -64,66 +64,103 @@ import ReactDOM from 'react-dom'
 //   )
 // }
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
+const Display = props => <div>{props.value}</div>
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
-const Display = ({counter}) => <div>{counter}</div>
+const ButtonTwo = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
-  const [counter, setCounter] = useState(0)
-  const [testCounter, testSetCounter] = useState("String")
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
+  const [valueTwo, setValueTwo] = useState(10)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-  const addLetterN = () => testSetCounter(testCounter + "n")
-  const resetToString = () => testSetCounter("String")
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
-  setTimeout(
-    () => {
-      setCounter(counter + 1)
-      testSetCounter(testCounter + "g")
-    },
-    1000
-  )
+  const handleClick = () => {
+    console.log('clicked the button')
+    setValue(0)
+  }
 
-  console.log('rendering...', counter)
+  const hello = (who) => () => {
+    console.log('hello', who)
+  }
+
+  const setToValueTwo = (newValue) => {
+    setValueTwo(newValue)
+  }
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <br />
+      <History allClicks={allClicks} />
 
       <br />
       <br />
       <br />
-
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-    
-      <br />
-      <br />
       <br />
 
-      <Display counter={testCounter} />
-      <Button handleClick={addLetterN} text="add n" />
-      <Button handleClick={resetToString} text="reset to string" />
+      <Display value={value} />
+
+      <button onClick={handleClick}>reset to zero</button>
+
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
+
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Display value={valueTwo} />
+      <ButtonTwo handleClick={() => setToValueTwo(1000)} text="thousand" />
+      <ButtonTwo handleClick={() => setToValueTwo(0)} text="reset" />
+      <ButtonTwo handleClick={() => setToValueTwo(valueTwo + 1)} text="increment" />
+
     </div>
   )
 }
